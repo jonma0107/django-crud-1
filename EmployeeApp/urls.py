3153cac2b604a3f80cdeba7ec5c311980d25b072
jonma0107 <jduplantier44@gmail.com>
2023-07-10 12:32:14 -0500

    API for employees

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -4,6 +4,9 @@ from EmployeeApp import views
 
 urlpatterns = [
     re_path(r'^department$', views.departmentApi),
-    re_path(r'^department/([0-9]+)$', views.departmentApi)
-    # re_path(r'^department$/([0-9]+)$', views.departmentApi)
+    re_path(r'^department/([0-9]+)$', views.departmentApi),
+    
+    re_path(r'^employee$', views.employeeApi),
+    re_path(r'^employee/([0-9]+)$', views.employeeApi),
+    
 ]
