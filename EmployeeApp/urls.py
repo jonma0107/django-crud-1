b25984be7dc7e554d13acf0900c7f7d5acd31ce5
jonma0107 <jduplantier44@gmail.com>
2023-07-10 12:01:06 -0500

    first commit

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,9 @@
+# from django.conf.urls import url
+from django.urls import re_path
+from EmployeeApp import views
+
+urlpatterns = [
+    re_path(r'^department$', views.departmentApi),
+    re_path(r'^department/([0-9]+)$', views.departmentApi)
+    # re_path(r'^department$/([0-9]+)$', views.departmentApi)
+]
